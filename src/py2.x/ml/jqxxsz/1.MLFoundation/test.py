b2286ad2078d940778bfb43fe48dcef3f16c480f
xiaoming3526 <272273581@qq.com>
2019-04-26 21:53:46 +0800

    test

--- src/py2.x/ml/jqxxsz/1.MLFoundation/test.py
+++ src/py2.x/ml/jqxxsz/1.MLFoundation/test.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+'''
+@File    :   test.py
+@Time    :   2019/04/26 21:52:56
+@Author  :   xiao ming 
+@Version :   1.0
+@Contact :   xiaoming3526@gmail.com
+@Desc    :   None
+@github  :   https://github.com/aimi-cn/AILearners
+'''
+
+# here put the import lib
+'''
+@description: 
+@param {type} 
+@return: 
+'''
+def test():
+    1
+
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
 import numpy as np
 np.zeros((2,3))
